e78fdcca8dd31135cb6a49946c39f62c7ade8181
azariacindy <cindysss099@gmail.com>
2023-11-07 22:12:53 +0700

    update code

---
+++ cinema07.java
@@ -0,0 +1,20 @@
+public class cinema07 {
+    public static void main(String[] args) {
+       String [][] audience = new String[4][2];
+
+       audience[0][0] = "Amin";
+       audience[0][1] = "Bena";
+       audience[1][0] = "Candra";
+       audience[1][1] = "Dela";
+       audience[2][0] = "Eka";
+       audience[2][1] = "Farhan";
+       audience[3][0] = "Gisel";
+
+       System.out.println(audience.length);
+
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Length of row " + (i + 1) + " : " + audience[1].length);
+        } 
+
+    }
+}
